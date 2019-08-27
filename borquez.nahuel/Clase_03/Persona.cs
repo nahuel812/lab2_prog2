a3cb68acc9bf684afb88889ddc359895dcea21a9
nahuel812 <36241474+nahuel812@users.noreply.github.com>
2019-08-27 11:30:48 -0300

    clase 3

---
+++ borquez.nahuel/Clase_03/Persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_03
+{
+    class Persona
+    {
+        public string nombre;
+        public string apellido;
+        public int dni;
+
+        public Persona(string nombre, string apellido, int dni)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.dni = dni;
+        }
+
+        public string Mostrar()
+        {
+            //al no se atributos staticos uso this
+            return this.nombre + " " + this.apellido + " " + this.dni + "\n"; 
+        }
+    }
+}
---
+++ borquez.nahuel/Clase_03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona MiPersona = new Persona("nahuel","borquez",42340058); ; //mi nueva instancia
+            /*
+            MiPersona.nombre = "nahuel";
+            MiPersona.apellido = "borquez";
+            MiPersona.dni = 42340058;
+            */
+            Console.WriteLine(MiPersona.Mostrar()); //uso el metodo mostrar de mi clase persona
+            Console.ReadLine();
+
+            Persona OtraPersona = new Persona("mario","perez",5666343);
+            /*
+            OtraPersona.nombre = "mario";
+            OtraPersona.apellido = "perez";
+            OtraPersona.dni = 45534455;
+            */
+            Console.WriteLine(OtraPersona.Mostrar());
+            Console.ReadLine();
+        }
+    }
+}
